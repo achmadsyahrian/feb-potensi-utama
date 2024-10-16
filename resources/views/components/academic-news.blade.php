9c8fc8148a11de61e636fa5994849ece3487e6f9
Achmad Syahrian <achmadsyahrian1@gmail.com>
2024-10-16 11:11:21 +0700

    fix

--- resources/views/components/academic-news.blade.php
+++ resources/views/components/academic-news.blade.php
@@ -12,7 +12,7 @@
                                     class="img-fluid" alt="{{$post->title}}">
                             @else
                                 <img src="{{ asset('landing/assets/img/logo-img/Logopotensiutama.png') }}"
-                                    style="height:240px; object-fit:cover;" alt="">
+                                    style="height:240px; object-fit:cover;" alt="Universitas Potensi Utama">
                             @endif
                         </a>
                     </div>
